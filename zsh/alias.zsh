8343751868db5c27a010fd4d013dd98af211e5cf
ComputerDuck <h.p.hachmann@icloud.com>
2023-08-02 14:24:32 +0200

    changes to tmux and new alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -14,3 +14,7 @@ alias py='python'
 alias jfd="fd --type f $(cat "$EXCLUDES" 2>/dev/null \ | sed "s/'/'\"'\"'/g" | awk "{printf \" -E '%s'\", "'$0}') | jfind --hints --show-key --select-both"
 
 alias tarcmd='cat ~/.config/help/tar'
+
+alias am="zsh ~/am/src/am.sh"
+# alias am="zsh ~/am/src/am.sh np & tmux split-window -h && tmux select-pane -R &&~/am/src/select.sh"
+
