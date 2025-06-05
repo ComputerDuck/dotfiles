d5e12495c6f6047f6682188c1ae5d991054bc6d4
Henrik Hachmann <henrik.hachmann@nerou.de>
2025-06-05 18:20:51 +0200

    add export path

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,37 +1,32 @@
 #change config dir
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=0
 
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
-#export PS1="%n@ %1~ $#"
-# export PS1="%{%F{red}%}%n%{%f%}@%{%F{green}%}%m %{%F{yellow}%}%~ %{$%f%} "
-#export PS1="%{%F{red}%}%m%{%f%}:%{%F{green}%}%n %{%F{yellow}%}%~ %{$%f%}%\ "
 
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-#path to own scripts
-export PATH=${PATH}:$HOME/.local/bin
-
-# export TERM=alacritty
 export TERM="xterm-256color"
 
 #homebrew
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/llvm@19/bin:$PATH"
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+#path to own scripts
+export PATH=${PATH}:$HOME/.local/bin
 
 export PATH="/usr/local/opt/postgresql@16/bin:$PATH"
+export PATH="/$HOME/scratch/zig/zig-x86_64-macos-0.14.1:$PATH"
+
 export PKG_CONFIG_PATH="/usr/local/opt/postgresql@16/lib/pkgconfig"
 export DYLD_FALLBACK_LIBRARY_PATH="/usr/local/lib"
 
 export GPG_TTY=$(tty)
 
-export ZSH_AUTOSUGGEST_MANUAL_REBIND=0
-
 export CC="/usr/local/opt/llvm@19/bin/clang"
 export CXX="/usr/local/opt/llvm@19/bin/clang++"
+export LDFLAGS="-L/usr/local/opt/llvm@19/lib"
+# export LDFLAGS="-L/usr/local/opt/llvm@19/lib/unwind -lunwind" // add this to clang.conf
+# export LDFLAGS="-L/usr/local/opt/llvm@19/lib/c++ -L/usr/local/opt/llvm@19/lib/unwind -lunwind" // add this clang++.conf
+export CPPFLAGS="-I/usr/local/opt/llvm@19/include"
+export DOCKER_HOST=unix:///$HOME/.colima/docker.sock
 
-# export TMUX_SCRIPTS="$HOME/.config/tmux/scripts"
-
-# export LIBRARY_PATH="/usr/local/opt/llvm@19/include/c++/v1"
